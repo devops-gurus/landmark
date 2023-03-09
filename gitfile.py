6780967a86ba23934a89f81c63d7a6c868ec264a
mosesweyori <126586480+mosesweyori@users.noreply.github.com>
2023-03-09 06:25:01 +0000

    Update gitfile.py

--- gitfile.py
+++ gitfile.py
@@ -3,3 +3,6 @@ I will keep practising this git and github shit
 until I get better you know 
 this is the only way to go for now
 thanks
+ok, this is me now trying to do a pull
+let's see it it works for now :) 
+and then thanks again
